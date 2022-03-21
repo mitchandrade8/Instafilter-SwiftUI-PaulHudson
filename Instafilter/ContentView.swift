24d53a25462a115ba0f4f59eb6fb25f682ec50c6
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2022-03-21 16:19:19 -0600

    Initial Commit

---
+++ Instafilter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Instafilter
+//
+//  Created by Mitch Andrade on 3/21/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
