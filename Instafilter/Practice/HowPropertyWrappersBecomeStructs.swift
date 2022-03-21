bceaf561224c5d72f354ff8020d05a9cc4de20bd
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2022-03-21 17:59:05 -0600

    second commit

---
+++ Instafilter/Practice/HowPropertyWrappersBecomeStructs.swift
@@ -0,0 +1,20 @@
+//
+//  HowPropertyWrappersBecomeStructs.swift
+//  Instafilter
+//
+//  Created by Mitch Andrade on 3/21/22.
+//
+
+import SwiftUI
+
+struct HowPropertyWrappersBecomeStructs: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HowPropertyWrappersBecomeStructs_Previews: PreviewProvider {
+    static var previews: some View {
+        HowPropertyWrappersBecomeStructs()
+    }
+}
---
+++ Instafilter/Practice/RespondingToStateChangesUsingOnChange.swift
@@ -0,0 +1,20 @@
+//
+//  RespondingToStateChangesUsingOnChange.swift
+//  Instafilter
+//
+//  Created by Mitch Andrade on 3/21/22.
+//
+
+import SwiftUI
+
+struct RespondingToStateChangesUsingOnChange: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RespondingToStateChangesUsingOnChange_Previews: PreviewProvider {
+    static var previews: some View {
+        RespondingToStateChangesUsingOnChange()
+    }
+}
